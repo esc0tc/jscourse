a764b386dc385424e77136be8f5d9766804f8ac4
esc0tc <ericschand@gmail.com>
2018-07-25 19:53:37 -0700

    tip calculator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,71 @@
+var mark = {
+  fullName: 'Mark Miller',
+  bills: [77,475,110,45],
+  calTips: function() {
+    this.tips = [];
+    this.finalValues = [];
 
+    for (var i = 0; i < this.bills.length; i++) {
+      //Determine percentage on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = .2;
+      } else if (bill >= 100 && bill<= 300) {
+        percentage = .1;
+      } else {
+        percentage = .25;
+      }
+
+      //Add result to corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+}
+
+var john = {
+  fullName: 'John Smith',
+  bills: [124,48,268,180,42],
+  calTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      //Determine percentage on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 50) {
+        percentage = .2;
+      } else if (bill >= 50 && bill<= 200) {
+        percentage = .15;
+      } else {
+        percentage = .1;
+      }
+
+      //Add result to corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+}
+
+function calcAverage(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
+}
+//do calculations
+john.calTips();
+mark.calTips();
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john);
+console.log(mark);
 
 
 
